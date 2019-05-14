caeb6224ead4958f4b47eb45caf9d52fa8e84dd6
Vitaliy Vakuliuk <vitaliyvak@gmail.com>
2019-05-14 13:02:33 +0300

    Added change in index.html, change index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ for (let i = 99; i >= 1; i--) {
         console.log()
     }
 }
+
+let submit = document.querySelector('#submit');
+submit.addEventListener('click', func);
+function func(){
+    document.getElementById('smile').style.display = "none";
+    submit.style.display = "none";
+}
